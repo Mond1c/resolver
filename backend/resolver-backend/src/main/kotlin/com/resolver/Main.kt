b78fe1a98c98af7b61427978cb0ad509a4ad04cf
Wolfram158 <wolframalpha7134@gmail.com>
2026-04-01 18:21:41 +0300

    conduct experiment

--- backend/resolver-backend/src/main/kotlin/com/resolver/Main.kt
+++ backend/resolver-backend/src/main/kotlin/com/resolver/Main.kt
@@ -3,41 +3,120 @@ package com.resolver
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.parameters.groups.provideDelegate
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.runBlocking
+import org.icpclive.cds.CommentaryMessagesUpdate
+import org.icpclive.cds.InfoUpdate
+import org.icpclive.cds.RunUpdate
 import org.icpclive.cds.adapters.addComputedData
+import org.icpclive.cds.adapters.applyEvent
 import org.icpclive.cds.adapters.contestState
-import org.icpclive.cds.api.ContestState
-import org.icpclive.cds.cli.CdsCommandLineOptions
+import org.icpclive.cds.api.*
+import org.icpclive.cds.scoreboard.getScoreboardCalculator
 
 class App : CliktCommand() {
-    private val cdsOptions by CdsCommandLineOptions()
+    private val resolverOptions by ResolverCommandLineOptions()
 
     override fun run() {
-        val scope = CoroutineScope(Dispatchers.Default)
-        val mtx = Mutex()
-        val states = mutableListOf<ContestState>()
-        scope.launch {
-            cdsOptions
-                .toFlow()
-                .addComputedData {
+        runBlocking {
+            val states = mutableListOf<ContestState>()
+            launch {
+                resolverOptions.toFlow().addComputedData {
                     firstToSolves = true
-                    submissionResultsAfterFreeze = false
+                    submissionResultsAfterFreeze = true
                 }
-                .contestState()
-                .collect {
-                    mtx.withLock {
-                        states.add(it)
+                    .contestState()
+                    .collect { state ->
+                        when (val lastEvent = state.lastEvent) {
+                            is CommentaryMessagesUpdate -> {}
+                            is InfoUpdate -> {
+                                if (lastEvent.newInfo.status is ContestStatus.OVER) {
+                                    states.add(state)
+                                    f(states)
+                                }
+                            }
+
+                            is RunUpdate -> {
+                                states.add(state)
+                            }
+                        }
                     }
-                }
+            }
         }
-        Thread.sleep(5000)
-        val contestState = states.last()
-        println(contestState.infoAfterEvent?.status)
     }
+}
+
+fun f(states: List<ContestState>) {
+    val endContestState = states.findLast { it.lastEvent is InfoUpdate }!!
+    val runs = states.filter { it.lastEvent is RunUpdate }
+    val notFrozenTemp = runs.filter {
+        (it.lastEvent as RunUpdate).newInfo.time < it.infoAfterEvent?.freezeTime!!
+    }
+    val notFrozenTemp1 = notFrozenTemp
+        .groupBy {
+            (it.lastEvent as RunUpdate).newInfo.teamId
+        }
+    val notFrozen =
+        notFrozenTemp1
+            .mapValues { state ->
+                state.value.groupBy {
+                    (it.lastEvent as RunUpdate).newInfo.problemId
+                }
+            }
+    val frozenTemp = runs.filter {
+        (it.lastEvent as RunUpdate).newInfo.time >= it.infoAfterEvent?.freezeTime!!
+    }
+    val frozen =
+        frozenTemp
+            .groupBy {
+                (it.lastEvent as RunUpdate).newInfo.teamId
+            }
+            .mapValues { state ->
+                state.value.groupBy {
+                    (it.lastEvent as RunUpdate).newInfo.problemId
+                }
+            }
+    val currentContestState = notFrozenTemp.last()
+    val contestInfo = currentContestState.infoBeforeEvent!!
+    val calculator = getScoreboardCalculator(contestInfo, OptimismLevel.NORMAL)
+    val rows = notFrozen.mapValues {
+        calculator.getScoreboardRow(
+            contestInfo,
+            notFrozen[it.key]!!.flatMap { it.value.map { (it.lastEvent as RunUpdate).newInfo } }
+        )
+    }
+    val ranks = calculator.getRanking(contestInfo, rows)
+    println(ranks.order)
+    val x =
+        frozenTemp.find {
+            (it.lastEvent as RunUpdate).newInfo.teamId.value == "spb428" &&
+                    ((it.lastEvent as RunUpdate).newInfo.result as RunResult.ICPC).verdict == Verdict.Accepted &&
+                    (it.lastEvent as RunUpdate).newInfo.problemId.value == "J"
+        }
+    val calculator1 = getScoreboardCalculator(x?.infoAfterEvent!!, OptimismLevel.NORMAL)
+    val rows1 = x.runsAfterEvent.values.groupBy { it.teamId }
+        .mapValues {
+            calculator1.getScoreboardRow(
+                x.infoAfterEvent!!,
+                it.value
+            )
+        }
+    val ranks1 = calculator1.getRanking(contestInfo, rows1)
+    println(ranks1.order)
+    println(ranks1.order.size)
+//    println(notFrozen.entries.joinToString("\n"))
+//    println(frozen.map { it.lastEvent })
+    val newContestState = currentContestState.applyEvent(x.lastEvent)
+    val contestInfo2 = newContestState.infoAfterEvent!!
+    val calculator2 = getScoreboardCalculator(contestInfo2, OptimismLevel.NORMAL)
+    val rows2 = newContestState.runsAfterEvent.values.groupBy { it.teamId }.mapValues {
+        calculator2.getScoreboardRow(
+            contestInfo2,
+            it.value
+        )
+    }
+    val ranks2 = calculator.getRanking(contestInfo2, rows2)
+    println(ranks2.order)
 }
 
 fun main(args: Array<String>) = App().main(args)
---
+++ backend/resolver-backend/src/main/kotlin/com/resolver/ResolverCommandLineOptions.kt
@@ -0,0 +1,5 @@
+package com.resolver
+
+import org.icpclive.cds.cli.CdsCommandLineOptions
+
+class ResolverCommandLineOptions : CdsCommandLineOptions()
