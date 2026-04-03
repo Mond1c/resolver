acca8a9325d297fd7fbdd1c41a3053a04a663a72
Wolfram158 <wolframalpha7134@gmail.com>
2026-04-04 01:29:29 +0300

    remove some strange things

--- backend/resolver-backend/resolution-logic/api/build.gradle.kts
+++ backend/resolver-backend/resolution-logic/api/build.gradle.kts
@@ -1,22 +1,9 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    application
 }
 
 group = "com.resolver"
 version = "1.0-logic-SNAPSHOT"
-
-application {
-    mainClass.set("com.resolver.MainKt")
-}
-
-tasks.jar {
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    manifest {
-        attributes["Main-Class"] = application.mainClass.get()
-    }
-}
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1")
