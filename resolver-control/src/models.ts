441ad4f2e68cbb785dfa67b27ef07858c509895e
Wolfram158 <wolframalpha7134@gmail.com>
2026-04-30 12:08:52 +0300

    remove leftover

--- resolver-control/src/models.ts
+++ resolver-control/src/models.ts
@@ -1,11 +1,4 @@
 import {TeamId} from "@shared/api";
-import {configureStore} from "@reduxjs/toolkit";
-import {widgetsReducer} from "@resolver/src/widgets";
-import scoreboardReducer from "@overlay/src/redux/contest/scoreboard";
-import {contestInfoReducer} from "@overlay/src/redux/contest/contestInfo";
-import rowReducer from "@resolver/src/redux/row";
-import problemReducer from "@resolver/src/redux/problem";
-import {useDispatch, useSelector} from "react-redux";
 
 export type ServerToControllerMessage =
     ServerToControllerMessage.VariantsToGoto
@@ -27,27 +20,3 @@ export namespace ServerToControllerMessage {
         variants: VariantToGoto[]
     }
 }
-
-// export const store = configureStore({
-//     reducer: {
-//         widgets: widgetsReducer,
-//         scoreboard: scoreboardReducer,
-//         contestInfo: contestInfoReducer,
-//         row: rowReducer,
-//         problem: problemReducer
-//     },
-//     middleware: (getDefaultMiddleware) => {
-//         return getDefaultMiddleware({
-//             immutableCheck: false,
-//             serializableCheck: false,
-//         });
-//     },
-//     devTools: import.meta.env.DEV,
-// });
-//
-// export type RootState = ReturnType<typeof store.getState>;
-//
-// export type AppDispatch = typeof store.dispatch;
-//
-// export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-// export const useAppSelector = useSelector.withTypes<RootState>();
